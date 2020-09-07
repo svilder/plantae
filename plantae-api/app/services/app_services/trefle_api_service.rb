84a60e163b1cec520848e0c396706347204c5c37
Sandra Vilder <sandra.vilder@gmail.com>
2020-09-07 11:00:29 +0200

    call with filter keyword

--- plantae-api/app/services/app_services/trefle_api_service.rb
+++ plantae-api/app/services/app_services/trefle_api_service.rb
@@ -1,19 +1,23 @@
 class TrefleApiService
   require 'httparty'
 
-  def initialize(params)
-    @keyword = params[:keyword]
+  def initialize(keyword)
+    @keyword = keyword
   end
 
   def call
-    r = HTTParty.get(
+    response = HTTParty.get(
       'https://trefle.io/api/v1/plants',
       query: {
-        "token": TREFLE_TOKEN
+        "token": "yF3YUN2Isv-0DH8Wuco-sx-3BKtN-D_x9mNAVZaUQxs",
+        "filter[common_name]": @keyword
       }
     )
-    r.parsed_response
+    # puts response.body
+    puts response.to_h.first(5)
+    # puts response.parsed_response
   end
 end
 
-
+trefleapiservice = TrefleApiService.new("sunflower")
+trefleapiservice.call
