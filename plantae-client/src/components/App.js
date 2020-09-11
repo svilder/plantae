08b453e95bba684430583eb4c1e49b427ef6134e
Sandra Vilder <sandra.vilder@gmail.com>
2020-09-11 16:01:04 +0200

    results component

--- plantae-client/src/components/App.js
+++ plantae-client/src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import SearchBar from './SearchBar';
+import Results from './Results';
 
 const App = () => {
   return (
   <div className="ui container" style={{marginTop: '32px'}}>
     <Header />
     <SearchBar />
+    <Results />
     <div>Chikidipah</div>
   </div>
   );
---
+++ plantae-client/src/components/Results.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Results = () => {
+  return (
+    <div>
+      <img />
+      <p className="description">
+        <h3>Title</h3>
+        <p>Description</p>
+      </p>
+    </div>
+  );
+};
+
+export default Results;
