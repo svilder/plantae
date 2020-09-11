c678652cc4e9b2b3c126405c754d05bf1b4cf3fb
Sandra Vilder <sandra.vilder@gmail.com>
2020-09-11 10:25:54 +0200

    simple app component

---
+++ plantae-client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+  <div className="ui container" style={{marginTop: '32px'}}>
+    <div>Dadadidadoup</div>
+  </div>
+  );
+};
+
+export default App;
